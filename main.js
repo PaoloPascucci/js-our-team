372bbd7a088a62812bfb1bf1bb8ddb7d45d02094
PaoloPascucci <pascuccip9@gmail.com>
2021-10-22 14:56:30 +0200

    Add Log

---
+++ main.js
@@ -0,0 +1,7 @@
+const team = [
+    { "nome": ['Marco', 'Alessia', 'Andrea', 'Chiara'] },
+    { "ruolo": ['Centravanti', 'Terzino', 'Punta', 'Portiere'] },
+    { "image": ['https://picsum.photos/images', 'https://picsum.photos/images', 'https://picsum.photos/images', 'https://picsum.photos/images'] }
+];
+
+console.log(team);
