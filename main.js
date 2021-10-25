abfb20a6e8fb97dae25f9aed795d4d57c6870a92
PaoloPascucci <pascuccip9@gmail.com>
2021-10-25 09:23:01 +0200

    Add for key

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const team = [
-    { "nome": ['Marco', 'Alessia', 'Andrea', 'Chiara'] },
-    { "ruolo": ['Centravanti', 'Terzino', 'Punta', 'Portiere'] },
-    { "image": ['https://picsum.photos/images', 'https://picsum.photos/images', 'https://picsum.photos/images', 'https://picsum.photos/images'] }
+    { 'nome': ['Marco', 'Alessia', 'Andrea', 'Chiara'] },
+    { 'ruolo': ['Centravanti', 'Terzino', 'Punta', 'Portiere'] },
+    { 'image': ['https://picsum.photos/images', 'https://picsum.photos/images', 'https://picsum.photos/images', 'https://picsum.photos/images'] }
 ];
+for (let key in team) {
 
-console.log(team);
+    console.log(team[key]);
+}
+//console.log();
